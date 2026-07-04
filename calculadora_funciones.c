3314871c0e71717c0cd95a0e2c4f977180d5d999
fab <efabian.guzman12@gmail.com>
2026-07-04 14:21:00 -0300

    Calculator with returns

--- calculadora_funciones.c
+++ calculadora_funciones.c
@@ -1,61 +1,61 @@
 #include <stdio.h>
-float sumar(float a, float b)
+float sum(float a, float b)
 {
     return a + b;
 }
-float restar(float a, float b)
+float rest(float a, float b)
 {
     return a - b;
 }
-float dividir(float a, float b)
+float divide(float a, float b)
 {
     return a / b;
 }
-float multiplicar(float a, float b)
+float multiply(float a, float b)
 {
     return a * b;
 }
 
 int main(void)
 {
-   int opcion;
+   int option;
    float num1, num2;
 
-   printf("Escoje la operacion.\n");
-   printf("1.Sumar\n");
-   printf("2.Restar.\n");
-   printf("3.Dividir\n");
-   printf("4.Multiplicar\n");
-   scanf("%d", &opcion);
+   printf("Choose the operation.\n");
+   printf("1.Sum.\n");
+   printf("2.Rest.\n");
+   printf("3.Divide.\n");
+   printf("4.Multiply.\n");
+   scanf("%d", &option);
 
-   if (opcion >= 1 && opcion <= 4)
+   if (option >= 1 && option <= 4)
    {
-    printf("Ingrese el primer numero.\n");
+    printf("Enter the first number: ");
     scanf("%f", &num1);
-    printf("Ingrese el segundo numero.\n");
+    printf("Enter the second number: ");
     scanf("%f", &num2);
 
-    switch (opcion) 
+    switch (option) 
     {
         case 1:
-        printf("Calculo: %.2f + %.2f: %.2f", num1, num2, sumar(num1, num2));
+        printf("Calculation: %.2f + %.2f: %.2f", num1, num2, sum(num1, num2));
         break;
 
         case 2:
-        printf("Calculo: %.2f - %.2f: %.2f", num1, num2, restar(num1, num2));
+        printf("Calculation: %.2f - %.2f: %.2f", num1, num2, rest(num1, num2));
         break;
 
         case 3:
-        printf("Calculo: %.2f / %.2f: %.2f", num1, num2, dividir(num1, num2));
+        printf("Calculation: %.2f / %.2f: %.2f", num1, num2, divide(num1, num2));
         break;
 
         case 4:
-        printf("Calculo: %.2f * %.2f: %.2f", num1, num2, multiplicar(num1, num2));
+        printf("Calculation: %.2f * %.2f: %.2f", num1, num2, multiply(num1, num2));
         break;
     }
    }
    else{
-    printf("Opcion Invalida. (Escoje 1 al 4)\n");
+    printf("Wrong Option. Choose a number beetwen 1 up to 100.\n");
    }
     return 0;
 }
