d8a1f8ed97ba54e883303356fbf8738f2e11b681
fabcctt <efabian.guzman12@gmail.com>
2026-07-14 13:24:36 -0300

    Updated recursion_factorial.cpp

--- cpp/recursion_factorial.cpp
+++ cpp/recursion_factorial.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 int factorial(int n)
 {
-    if (n == 1)
+    if(n == 1)
     {
         return 1;
     }
-    return n * factorial (n - 1);
+    return n * factorial(n - 1);
 }
 int main(void)
 {
