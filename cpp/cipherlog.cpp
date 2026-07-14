94f8782ed2e91eaed12f506c11780b131c2648e2
fabcctt <efabian.guzman12@gmail.com>
2026-07-14 13:23:46 -0300

    Updated cipherlog.cpp

--- cpp/cipherlog.cpp
+++ cpp/cipherlog.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <string>
 #include <fstream>
-
 struct PasswordEntry {
     std::string site;
     std::string password;
@@ -9,74 +8,86 @@ struct PasswordEntry {
 int main(void)
 {
     int option;
-    int key = 'K';
+    char key = 'K';
     bool running = true;
 
-    while(running) {
-    std::cout << "--- CipherLog System ---\n";
-    std::cout << "1. Save a password\n";
-    std::cout << "2. See saved password\n";
-    std::cout << "3. Exit\n";
-    std::cout << "Choose an option: ";
-    std::cin >> option;
-    std::cin.ignore();
-
-    switch(option)
+    while(running)
     {
-        case 1:
+        std::cout << "========================" << std::endl;
+        std::cout << "Welcome to CipherLog." << std::endl;
+        std::cout << "========================" << std::endl;
+        std::cout << "1.Save Password." << std::endl;
+        std::cout << "2.See Password." << std::endl;
+        std::cout << "3.Exit." << std::endl;
+        std::cout << "========================" << std::endl;
+        std::cout << "Choose an Option." << std::endl;
+        if(!(std::cin >> option))
         {
-        PasswordEntry newEntry;
-        std::cout << "Site: "; std::getline(std::cin, newEntry.site);
-        std::cout << "Password: "; std::getline(std::cin, newEntry.password);
-
-        for(size_t i = 0; i < newEntry.password.length(); i++)
-        {
-            newEntry.password[i] = newEntry.password[i] ^ key;
+            std::cin.clear();
+            std::cin.ignore(10000, '\n');
+            std::cout << "Wrong Option. (Choose between 1 up to 3.)\n\n" << std::endl;
+            continue;
         }
-        std::ofstream outFile("vault.dat", std::ios::app);
-        if(outFile.is_open())
-        {
-            outFile << newEntry.site << std::endl;
-            outFile << newEntry.password << std::endl;
-            outFile.close();
-            std::cout << "Saved Sucesfully.\n";
-        }
-        break;
-        }
-
-        case 2:
-        {
-        std::ifstream inFile("vault.dat");
-        std::string line;
-        bool isPassword = false;
+        std::cin.ignore(10000, '\n');
         
-        while(std::getline(inFile, line))
+        switch(option)
         {
-            if(isPassword)
+            case 1:
             {
-                for(size_t i = 0; i < line.length(); i++)
+                PasswordEntry newEntry;
+                std::cout << "Site: "; std::getline(std::cin, newEntry.site);
+                std::cout << "Password: "; std::getline(std::cin, newEntry.password);
+
+                for(size_t i = 0; i < newEntry.password.length(); i++)
                 {
-                    line[i] = line[i] ^ key;
+                    newEntry.password[i] = newEntry.password[i] ^ key;
                 }
-                std::cout << "Password: " << line << "\n";
-                isPassword = false;
+                std::ofstream outFile("name.txt", std::ios::app);
+                if(outFile.is_open())
+                {
+                    outFile << newEntry.site << std::endl;
+                    outFile << newEntry.password << std::endl;
+                    outFile.close();
+                    std::cout << "Saved Sucesfully." << std:: endl;
+                }
+            break;
             }
-            else
+            case 2:
             {
-                std::cout << "site: " << line << "\n";
-                isPassword = true;
+            std::ifstream inFile("name.txt");
+            std::string line;
+            bool isPassword = false;
+            while(std::getline(inFile, line))
+            {
+                if(isPassword)
+                {
+                    for(size_t i = 0; i < line.length(); i++)
+                    {
+                        line[i] = line[i] ^ key;
+                    }
+                    std::cout << "Password: " << line << std::endl;
+                    std::cout << "------------------\n";
+                }
+                else
+                {
+                    std::cout << "------------------\n";
+                    std::cout << "Site: " << line << std::endl;
+                }
+                isPassword = !isPassword;
             }
-        }
-        break;
-        }
-        case 3:
-        {
+
+            break;
+            }
+            case 3:
+            {
             running = false;
             break;
+            }
+            default:
+            {
+                std::cout << "Wrong Number.\n\n" << std::endl;
+            }
         }
-        default:
-        std::cout << "Wrong option\n";
-    }
     }
     return 0;
 }
